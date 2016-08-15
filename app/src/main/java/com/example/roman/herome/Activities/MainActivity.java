f13b2d065d67a25d75e2e38b1da6d7635958e726
Roman Puzey <Roman@romans-mbp.attlocal.net>
2016-08-15 00:56:51 -0500

    initial commit

---
+++ app/src/main/java/com/example/roman/herome/Activities/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.roman.herome.Activities;
+
+import android.net.Uri;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.roman.herome.Fragments.MainFragment;
+import com.example.roman.herome.Fragments.PickPowerFragment;
+import com.example.roman.herome.R;
+
+public class MainActivity extends AppCompatActivity implements MainFragment.MainFragmentInteractionListener, PickPowerFragment.PickPowerInteractionListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        FragmentManager manager = getSupportFragmentManager();
+        Fragment fragment = manager.findFragmentById(R.id.fragment_container);
+
+        if(fragment == null)
+        {
+            fragment = new MainFragment();
+            manager.beginTransaction().add(R.id.fragment_container, fragment).commit();
+
+        }
+    }
+
+    public void loadPickPowerScreen()
+    {
+        PickPowerFragment pickPowerFragment = new PickPowerFragment();
+        this.getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, pickPowerFragment).addToBackStack(null).commit();
+
+    }
+
+    @Override
+    public void onMainFragmentInteraction(Uri uri) {
+
+    }
+
+    @Override
+    public void onPickPowerFragmentInteraction(Uri uri) {
+
+    }
+}
